5fabdcd6be792c3d18fdce15bb9ea1e17a789888
Felipe Presente <felipe.presente@aluno.ifsp.edu.br>
2025-09-04 15:06:55 -0300

    landing page beta1.1

--- assets/js/idioma.js
+++ assets/js/idioma.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
     const br = document.getElementById("br")
     const en = document.getElementById("en")
+    const spanish = document.getElementById("spanish")
     const item1 = document.getElementById("item1")
     const item2 = document.getElementById("item2")
     const item3 = document.getElementById("item3")
@@ -26,13 +27,35 @@ document.addEventListener("DOMContentLoaded", function() {
     en.onclick = function() {
 
         en.classList.toggle("hidden")
+        spanish.classList.toggle("hidden")
+
+        landingPageP.innerText = "Intercambio, Lengua y Cultura"
+        entrar.innerText = "Entrar"
+        criarConta.innerText ="Crear cuenta"
+        acessar.innerText = "Acesso"
+        item1.innerText = "Artículo 1"
+        item2.innerText = "Artículo 2"
+        item3.innerText = "Artículo 3"
+        item4.innerText = "Artículo 4"
+
+    }
+
+
+    spanish.onclick = function() {
+
+        spanish.classList.toggle("hidden")
         br.classList.toggle("hidden")
 
         landingPageP.innerText = "Intercâmbio, Linguagem e Cultura"
         entrar.innerText = "Entrar"
         criarConta.innerText ="Criar conta"
         acessar.innerText = "Acessar"
+        item1.innerText = "Item 1"
+        item2.innerText = "Item 2"
+        item3.innerText = "Item 3"
+        item4.innerText = "Item 4"
 
     }
+    
 
 })
