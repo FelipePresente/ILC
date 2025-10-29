dd1c6a6131cfd0c8b9e3e6d7c2c885c023f6bb2f
Felipe Presente <felipe.presente@aluno.ifsp.edu.br>
2025-10-29 10:21:37 -0300

    tentatativa de resposividade em andamento

--- assets/js/criarConta.js
+++ assets/js/criarConta.js
@@ -5,6 +5,8 @@ document.addEventListener("DOMContentLoaded", function(){
     const criarConta = document.querySelector("#criarConta")
     const body = document.querySelector("body")
     const blackscreen = document.querySelector("#blackscreen")
+    const visibility = document.querySelector("#visibility")
+    const visibilityOff = document.querySelector("#visibilityOff")
 
     retornar.addEventListener("click", function(){
         criarContaMenu.classList.toggle("hidden")
@@ -18,6 +20,16 @@ document.addEventListener("DOMContentLoaded", function(){
         body.classList.toggle("overflow-hidden")
         blackscreen.classList.toggle("bg-black/60")
         blackscreen.classList.toggle("z-0")
+    })
+
+    visibility.addEventListener("click", function(){
+        visibility.classList.toggle("hidden")
+        visibilityOff.classList.toggle("hidden")
+    })
+
+    visibilityOff.addEventListener("click", function(){
+        visibility.classList.toggle("hidden")
+        visibilityOff.classList.toggle("hidden")
     })
 
 })
